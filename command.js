9cf5d262710fd3246981ca7026329b079b8f2fe5
yagnesh <yagnesh6202patel@gmail.com>
2020-12-28 14:45:44 +0530

    This is last change

---
+++ command.js
@@ -0,0 +1,6 @@
+git status  //check for status
+git init  // initialize a master branch
+rm -rf .git //delete the master branch
+git add  index.html  // add any file
+git add --a  //add all file
+git commit -m "this is change"  //commit
