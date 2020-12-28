85fc6a46e30f6f301593a942e90b7876b84412ee
yagnesh <yagnesh6202patel@gmail.com>
2020-12-28 15:38:57 +0530

    This is last change i do it

--- command.js
+++ command.js
@@ -6,6 +6,12 @@ git add --a  //add all file
 git commit -m "this is change"  //commit
 touch  error.log//(filename.extension name)  //Creating a blank file
 
-//Git ignore file
 touch .gitignore  //Creating .gitignore
+//Git ignore file
+//don't add the ignoring file in git
+practice.txt
 error.log  //file name - this is ignoring by git
+*.log //All log file ignoring by git
+dir/ //All dir foder ignoring by git
+/dir/ //outer dir folder ignoring by git
+/static/helllo.txt //this path ignoring by git 
